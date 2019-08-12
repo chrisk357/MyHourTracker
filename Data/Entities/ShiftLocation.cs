a5f0902a9fffe21106e0a2adf7fb2514cd0039e3
Chris Desktop <chrisk357@gmail.com>
2019-08-12 16:51:51 -0500

    First Commit

---
+++ Data/Entities/ShiftLocation.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHourTracker.Data.Entities
+{
+    public class ShiftLocation
+    {
+        public int Id { get; set; }
+        public string LocationName { get; set; }
+    }
+}
---
+++ Data/Entities/TimeCard.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyHourTracker.Data.Entities
+{
+    public class TimeCard
+    {
+        public int Id { get; set; }
+        public int ClockInH { get; set; }
+        public int ClockInM { get; set; }
+        public int ClockOutH { get; set; }
+        public int ClockOutM { get; set; }
+        public DateTime ShiftDate { get; set; }
+    }
+}
